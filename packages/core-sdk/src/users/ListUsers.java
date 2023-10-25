fb6b72316553a4cdfcaa7783ce38607bcb056cc2
Étienne <e-begon@hotmail.fr>
2023-10-25 22:00:23 +0200

    wip: system is changing constantly

--- packages/core-sdk/src/users/ListUsers.java
+++ packages/core-sdk/src/users/ListUsers.java
@@ -1,7 +1,7 @@
+package users;
+
 import java.util.List;
 import java.util.UUID;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
 
 import entities.User;
 import exceptions.UnknownFailureException;
@@ -9,13 +9,8 @@ import storages.UsersQueries;
 
 public class ListUsers {
     
-    public List<User> execute(UsersQueries usersQueries) throws UnknownFailureException {
-        try {
-            CompletableFuture<List<User>> usersFuture = usersQueries.selectUsers();
-            return usersFuture.get();
-        } catch (InterruptedException | ExecutionException e) {
-            throw new UnknownFailureException(e.getMessage(), e.getCause());
-        }
+    public static List<User> execute(UsersQueries usersQueries) throws UnknownFailureException {
+        return usersQueries.listUsers();
     }
 
 }
